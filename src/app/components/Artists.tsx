3b2eeb80b7c25bd84571a8a960661e056196b885
beasiteri <siteribea@gmail.com>
2025-03-12 19:38:58 +0100

    Fetch artists data & display in MUI DataGrid with pagination

--- src/app/components/Artists.tsx
+++ src/app/components/Artists.tsx
@@ -1,10 +1,50 @@
-import { Pagination } from '@mui/material';
+"use client";
+
+import { useEffect, useState } from 'react';
+import { DataGrid } from '@mui/x-data-grid';
+import { CircularProgress } from '@mui/material';
+import { fetchArtists } from '../services/api';
+import { ApiResponse } from '../services/interfaces';
+import { artistColumns } from "./artistColumns";
 
 export const Artists = () => {
+  const [loading, setLoading] = useState<boolean>(false);
+  const [artists, setArtists] = useState<ApiResponse ['data']>([]);
+  const [pagination, setPagination] = useState<ApiResponse['pagination']  | null>(null);
+  const [page, setPage] = useState(1);
+  const perPage = 50;
+
+  useEffect(() => {
+    const loadArtists = async () => {
+      setLoading(true);
+      try {
+        const {data: artists, pagination} = await fetchArtists({ page, per_page: perPage });
+        setArtists(artists);
+        setPagination(pagination);
+      } catch (err) {
+        console.log('Error fething artists: ', err);
+      } finally {
+        setLoading(false);
+      }
+    }
+    loadArtists();
+  }, [page]);
+  
   return (
     <>
-      <div>TEST</div>
-      <Pagination count={10} variant="outlined" shape="rounded" />
+      {loading ? 
+        <CircularProgress color="secondary" /> : 
+        <DataGrid
+          style={{ height: "100%", width: "100%" }}
+          rows={artists}
+          columns={artistColumns}
+          paginationMode="server"
+          paginationModel={{ pageSize: perPage, page: page - 1 }}
+          rowCount={pagination?.total_items || 0}
+          pageSizeOptions={[perPage]}
+          onPaginationModelChange={(params) => setPage(params.page + 1)}
+        />
+      }
     </>
   );
 }
