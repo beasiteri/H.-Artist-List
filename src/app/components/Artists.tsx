724cb3c3d8b53973daea29e8db07ad3c225fef30
beasiteri <siteribea@gmail.com>
2025-03-12 13:20:29 +0100

    Add Artists component with MUI pagination

---
+++ src/app/components/Artists.tsx
@@ -0,0 +1,10 @@
+import { Pagination } from '@mui/material';
+
+export const Artists = () => {
+  return (
+    <>
+      <div>TEST</div>
+      <Pagination count={10} variant="outlined" shape="rounded" />
+    </>
+  );
+}
