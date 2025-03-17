1cce00e611ecca618e9d6c049e3759f8797d1efb
beasiteri <siteribea@gmail.com>
2025-03-17 21:47:30 +0100

    Remove unused utility methods

--- src/app/utils/utility.tsx
+++ src/app/utils/utility.tsx
@@ -1,11 +1,3 @@
-export const sortByName = (record1: { name: string }, record2: { name: string }) => {
-  return record1.name.localeCompare(record2.name);
-};
-
-export const sortByAlbumCount = (record1: { albumCount: number }, record2: { albumCount: number }) => {
-  return record1.albumCount - record2.albumCount;
-};
-
 export const filterImages = (artists: { portrait: string }[]) => {
   return artists.filter((artist) => artist.portrait);
 }
